d078becc4cc8d32fee2dc977d8a10d0c2eff83f8
nerdsrescueme <nerdsrescueme@gmail.com>
2012-01-19 20:58:02 -0500

    Small fixes

--- fuel/app/classes/controller/admin.php
+++ fuel/app/classes/controller/admin.php
@@ -1,15 +1,12 @@
 <?php
 
-class Controller_Admin extends Controller
-{
-	public function before()
-	{
-		if(!Input::is_ajax())
-		{
+class Controller_Admin extends Controller {
+
+	public function before() {
+		if(!Input::is_ajax()) {
 			Session::set_flash('info', 'You may only access administration areas from the control bar interface.');
 			return Response::redirect('/');
 		}
-
 		return parent::before();
 	}
 }
--- fuel/app/classes/controller/cms.php
+++ fuel/app/classes/controller/cms.php
@@ -5,67 +5,55 @@ class TempUser
 	public function is_admin() { return true; }
 }
 
-class Controller_Cms extends Controller
-{
+class Controller_Cms extends Controller {
+
 	public $site;
 	public $user;
 	public $theme;
 	public $template;
 
-	public function action_home()
-	{
+	public function action_home() {
 		$this->template->content = $this->theme->view('home');
 	}
 
-	public function action_catch()
-	{
-		if(!$page = Model_Page::find_current())
-		{
+	public function action_catch() {
+		if(!$page = Model_Page::find_current()) {
 			$this->response->set_status(404);
 			$this->template->content = $this->theme->view('404');
 			return;
 		}
-
 		$this->template->content = 'Content for page ['.$page->title.'].';
 	}
 
-	public function action_login()
-	{
-		if(Input::method() === 'POST')
-		{
+	public function action_login() {
+		if(Input::method() === 'POST') {
 			Session::set_flash('success', 'Welcome, you have successfully logged in.');
 			return Response::redirect('/');
 		}
-
 		return Response::forge(View::forge('cms/login'));
 	}
 
-	public function action_logout()
-	{
+	public function action_logout() {
 		return Response::redirect('/');
 	}
 
-	public function before()
-	{
+	public function before() {
 		$this->site     = Model_Site::find_or_create_current();
 		$this->user     = new TempUser();
 		$this->theme    = Theme::instance();
 		$this->template = $this->theme->view('template');
 		$active_theme   = $this->theme->active();
 		Asset::add_path('themes/'.$active_theme['name'].'/assets/');
-
 		return parent::before();
 	}
 
-	public function after($response)
-	{
+	public function after($response) {
 		if($response instanceof Response) return $response;
 
 		// If AJAX, return the content without the layout
 		$body = Input::is_ajax() ? $this->template->content : $this->template;
 
-		if($this->user and $this->user->is_admin())
-		{
+		if($this->user and $this->user->is_admin()) {
 			Asset::js(array(
 				'bootstrap-transition.js',
 				'bootstrap-modal.js',
@@ -81,7 +69,6 @@ class Controller_Cms extends Controller
 		}
 
 		$this->response->body = $body;
-
 		return parent::after($this->response);
 	}
 }
