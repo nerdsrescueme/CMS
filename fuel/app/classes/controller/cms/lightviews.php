7ca832c982431e5247a81f619ef72c3c94ae6244
Frank Bardon Jr <frank@nerdsrescue.me>
2012-01-27 08:49:20 -0500

    Update fuel/app/classes/controller/cms/lightviews.php

--- fuel/app/classes/controller/cms/lightviews.php
+++ fuel/app/classes/controller/cms/lightviews.php
@@ -1,8 +1,9 @@
 <?php
 
-class Controller_Cms_Lightviews extends Controller {
-
-	public function action_about() {
+class Controller_Cms_Lightviews extends Controller
+{
+	public function action_about()
+	{
 		return Response::forge(View::forge('cms/lightviews/about'));
 	}
 }
