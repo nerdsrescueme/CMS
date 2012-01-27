7e6954d7431e7227a4bd0bc63993cab8979d7913
Frank Bardon Jr <frank@nerdsrescue.me>
2012-01-27 08:49:46 -0500

    Update fuel/app/classes/controller/cms/modals.php

--- fuel/app/classes/controller/cms/modals.php
+++ fuel/app/classes/controller/cms/modals.php
@@ -1,24 +1,29 @@
 <?php
 
-class Controller_Cms_Modals extends Controller {
-
-	public function action_character() {
+class Controller_Cms_Modals extends Controller
+{
+	public function action_character()
+	{
 		return Response::forge(View::forge('cms/modals/character'));
 	}
 
-	public function action_htmleditor() {
+	public function action_htmleditor()
+	{
 		return Response::forge(View::forge('cms/modals/htmleditor'));
 	}
 
-	public function action_link() {
+	public function action_link()
+	{
 		return Response::forge(View::forge('cms/modals/link'));
 	}
 
-	public function action_media() {
+	public function action_media()
+	{
 		return Response::forge(View::forge('cms/modals/media'));
 	}
 
-	public function action_table() {
+	public function action_table()
+	{
 		return Response::forge(View::forge('cms/modals/table'));
 	}
 }
