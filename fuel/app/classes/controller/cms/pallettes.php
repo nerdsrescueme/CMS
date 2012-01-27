02b2223a8f20a272601a4e7548a81f3b5b3fce1d
Frank Bardon Jr <frank@nerdsrescue.me>
2012-01-27 08:50:04 -0500

    Update fuel/app/classes/controller/cms/pallettes.php

--- fuel/app/classes/controller/cms/pallettes.php
+++ fuel/app/classes/controller/cms/pallettes.php
@@ -1,12 +1,14 @@
 <?php
 
-class Controller_Cms_Pallettes extends Controller {
-
-	public function action_backcolor() {
+class Controller_Cms_Pallettes extends Controller
+{
+	public function action_backcolor()
+	{
 		return Response::forge(View::forge('cms/pallettes/backcolor'));
 	}
 
-	public function action_forecolor() {
+	public function action_forecolor()
+	{
 		return Response::forge(View::forge('cms/pallettes/forecolor'));
 	}
 }
