af4f57f8be8d036ea2b36133476fc0216cc0e8e8
nerdsrescueme <nerdsrescueme@gmail.com>
2012-08-02 18:47:22 -0400

    Added comments to program pages.

--- public/themes/tbp/programs.php
+++ public/themes/tbp/programs.php
@@ -18,4 +18,17 @@
 	</div>
 	<!-- end tb-similar --></div>
 </div>
+
+<div id="disqus_thread" class="span8"></div>
+<script type="text/javascript">
+    var disqus_shortname = 'teambuildersplus';
+    var disqus_developer = 1;
+
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view comments.</noscript>
 </article>
