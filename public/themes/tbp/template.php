88919e46a86b28ba1b9b1632fe3bd063b4b61838
nerdsrescueme <nerdsrescueme@gmail.com>
2012-08-02 20:29:16 -0400

    Added Google Analytics Tracking Code

--- public/themes/tbp/template.php
+++ public/themes/tbp/template.php
@@ -145,7 +145,7 @@
 <script src="/themes/tbp/assets/js/bootstrap-carousel.js"></script>
 <script src="/themes/tbp/assets/js/bootstrap-dropdown.js"></script>
 
-<script>
+<script type="text/javascript">
 $(document).ready(function() {
 	$('#slider').carousel({ interval: 9000 })
 	$('#social a[title]').tooltip({ placement: 'bottom' })
@@ -160,6 +160,16 @@ $(document).ready(function() {
 			$(this).animate({width: '18px', height: '18px'})
 		})
 })
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-972486-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 </script>
 
 </body>
