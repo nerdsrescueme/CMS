56b04494fa4b85bfcccffed3783a37cf63815550
nerdsrescueme <nerdsrescueme@gmail.com>
2012-08-02 20:08:05 -0400

    Adjusted 2 column layout

--- public/themes/tbp/two-columns.php
+++ public/themes/tbp/two-columns.php
@@ -5,19 +5,12 @@
 	<p>Main headline</p>
 <!-- end description --></div>
 
-<div class="span4" id="left-column" data-editable="local"><!-- start left-column -->
+<div class="span6" id="left-column" data-editable="local"><!-- start left-column -->
 	<p>Left column content</p>
 <!-- end left-column --></div>
 
-<div class="span4" id="right-column" data-editable="local"><!-- start right-column -->
+<div class="span6" id="right-column" data-editable="local"><!-- start right-column -->
 	<p>Right column content</p>
 <!-- end right-column --></div>
 
-<div id="sidebar" class="span3 offset1">
-
-	<div id="about-navigation" class="navigation snippet" data-editable="global"><!-- sidebar-content -->
-		<p>Sidebar content</p>
-	<!-- end sidebar-content --></div>
-
-</div>
 </article>
