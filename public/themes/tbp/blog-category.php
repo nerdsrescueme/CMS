0483f70ffd21d5c0098d2524a911a7d951724385
nerdsrescueme <nerdsrescueme@gmail.com>
2012-08-02 23:27:35 -0400

    Updated blog templates

--- public/themes/tbp/blog-category.php
+++ public/themes/tbp/blog-category.php
@@ -8,8 +8,17 @@
 	<div id="blog-image" class="snippet" data-editable="local"><!-- start blog-image -->
 		<img src="http://placehold.it/210x160" class="bordered">
 	<!-- end blog-image --></div>
+
 	<div id="blog-description" class="navigation snippet" data-editable="local"><!-- start blog-description -->
 	<p>This is simply a short description of this blog pages' content, and what to expect in this category.</p>
 	<!-- end blog-description --></div>
+	
+	<div id="blog-categories" class="navigation snippet" data-editable="global"><!-- start blog-categories -->
+	<ol>
+		<li class="active"><a href="#">History</a></li>
+		<li><a href="#">Secondary</a></li>
+		<li><a href="#">Tertiary</a></li>
+	</ol>
+	<!-- end blog-categories --></div>
 </div>
 </article>
