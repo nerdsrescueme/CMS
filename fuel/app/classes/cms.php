9f85baabab4c2c8d189f61f9af847428e2d8435b
Frank Bardon Jr <frank@nerdsrescue.me>
2012-10-22 12:09:36 -0300

    Twitter url changed for basic API.

--- fuel/app/classes/cms.php
+++ fuel/app/classes/cms.php
@@ -38,7 +38,8 @@ class CMS {
 	
 	public static function twitter($username, $num_posts)
 	{
-		$json = file_get_contents("http://twitter.com/status/user_timeline/{$username}.json?count={$num_posts}", true);
+		$json = file_get_contents("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={$username}&count={$num_posts}", true);
+		//$json = file_get_contents("http://twitter.com/status/user_timeline/.json?count=", true);
 		$json = json_decode($json, true);
 		
 		// Autodetect urls
