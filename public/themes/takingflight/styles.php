3b71a1ab58f6257416f1359f9ca8606192760cb9
nerdsrescueme <nerdsrescueme@gmail.com>
2012-10-30 11:50:45 -0400

    Added styles to tf theme

---
+++ public/themes/takingflight/styles.php
@@ -0,0 +1,32 @@
+<article id="content" class="row">
+<div class="span6" id="main" data-editable="local"><!-- start main -->
+	<h2>Main Headline</h2>
+	<p>Body content</p>
+<!-- end main --></div>
+
+<div id="sidebar" class="span5 offset1">
+	<div id="tf-image" class="navigation snippet" data-editable="local"><!-- start tf-image -->
+		<img src="http://placehold.it/320x180" class="bordered">
+	<!-- end tf-image --></div>
+	<div id="tf-styles" class="navigation snippet" data-editable="local"><!-- start tf-styles -->
+	<h3>Similar Programs</h3>
+	<ol>
+		<li><a href="#">First</a></li>
+		<li><a href="#">Second</a></li>
+	</ol>
+	<!-- end tf-styles --></div>
+</div>
+
+<div id="disqus_thread" class="span8"></div>
+<script type="text/javascript">
+    var disqus_shortname = 'takeflightlearning';
+    var disqus_developer = 1;
+
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view comments.</noscript>
+</article>
