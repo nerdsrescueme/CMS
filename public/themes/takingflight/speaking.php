f1558b62cbdcdd96505fccf67ec0f049d8fca7c2
nerdsrescueme <nerdsrescueme@gmail.com>
2012-11-01 20:41:26 -0400

    Added speaking layout

---
+++ public/themes/takingflight/speaking.php
@@ -0,0 +1,14 @@
+<article id="content" class="row">
+<div class="span8" id="main" data-editable="local"><!-- start main -->
+	<h2>Main Headline</h2>
+	<p>Body content</p>
+<!-- end main --></div>
+
+<div id="sidebar" class="span3 offset1">
+	<div id="tf-speaking-nav" class="navigation snippet" data-editable="global"><!-- start tf-speaking-nav -->
+	<ol>
+		<li class="active"><a href="#">Item 1</a></li>
+	</ol>
+	<!-- end tf-speaking-nav --></div>
+</div>
+</article>
