b1772705f001d70a66ecd7cecfdb1af9b662bab7
nerdsrescueme <nerdsrescueme@gmail.com>
2012-11-01 22:27:12 -0400

    TF Theme edits

---
+++ public/themes/takingflight/landing.php
@@ -0,0 +1,27 @@
+<section class="row">
+	<div class="span5 centered-content">
+		<img src="assets/img/taking-flight/book-large.jpg" alt="Taking Flight with DISC book cover">
+	</div>
+	<div id="tf-landing-copy" data-editable="local" class="span6"><!-- start tf-landing-copy -->
+		<h3 class="exciting">Thanks for visiting us from the <u>Taking Flight!</u> website.</h3>
+		<p>Marketing copy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida hendrerit adipiscing. Nunc metus eros, vehicula id blandit at, <span class="label success">amazing</span> nulla. Aenean auctor mauris sed nulla ultricies eget convallis erat <span class="label success">wonderful</span>. Morbi quam dolor, cursus in commodo vel, pretium sed neque. Nunc a mauris id sapien vestibulum elementum eget non orci. Ut vel enim ut erat aliquet scelerisque.</p>
+		<ul>
+			<li>Choose us because we are amazing!</li>
+			<li>There are definitely more reasons than that</li>
+			<li>I bet you're interested in finding out too</li>
+			<li>Well, that's a good thing because I'm definitely</li>
+			<li>Here to help you figure that out</li>
+		</ul>
+		<p>&nbsp;</p>
+		<div>
+			<a href="#" class="btn success">Click here for a special offer!</a>
+			<a href="#" class="btn">Ignore offer and go home</a>
+		</div>
+	<!-- end tf-landing-copy --></div>
+</section>
+
+<footer id="footer-text" class="row">
+<div class="span12">
+	<p><a href="/">go to our home page</a></p>
+</div>
+</footer>
