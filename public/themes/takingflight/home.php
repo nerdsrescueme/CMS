7bde2f4da206b3f59cb0c09befd513132e880a31
nerdsrescueme <nerdsrescueme@gmail.com>
2012-11-01 21:43:24 -0400

    TF Theme edits

--- public/themes/takingflight/home.php
+++ public/themes/takingflight/home.php
@@ -1,10 +1,30 @@
-<section id="recent" class="row">
-	<div id="tf-overview" class="span6" data-editable="global"><!-- start tf-overview -->Content<!-- end tf-overview --></div>
-	<div id="tf-extra" class="span6" data-editable="local"><!-- start tf-extra -->Content<!-- end tf-extra --></div>
+<section id="overview" class="row">
+	<div id="tf-overview" data-editable="local" class="span6 offset3"><!-- start tf-overview -->Content<!-- end tf-overview --></div>
 </section>
 
-<section id="feature" class="row">
-	<div id="tf-book" class="span4" data-editable="local"><!-- start tf-book -->Content<!-- end tf-book --></div>
-	<div id="tf-training" class="span4" data-editable="local"><!-- start tf-training -->Content<!-- end tf-training --></div>
-	<div id="tf-speaking" class="span4" data-editable="local"><!-- start tf-speaking -->Content<!-- end tf-speaking --></div>
+<section id="recent" class="row">
+	<div class="span6">
+		<h4 class="underlined">Testimonials</h4>
+		<div id="testimonials" class="carousel slide">
+			<div class="carousel-inner">
+				<div class="item active">
+					<blockquote>"I enjoyed the exercises because they made the concepts more realistic and applicable."</blockquote>
+					<p>&mdash; Cheryl Davis, <b>Campbell Soup Company</b></p>
+				</div>
+				<div class="item">
+					<blockquote>"The quality and the usefulness of the 360 reports and the fact that we had group data added greatly to the overall leadership experience for our most senior people."</blockquote>
+					<p>&mdash; Ian Cormack, <b>Campbell Soup Company</b></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="span6">
+		<h4 class="underlined"><a href="http://twitter.com/DISCStyles">Recent Tweets</a></h4>
+		<ol id="tweets">
+		<?php $twitter = CMS::twitter('DISCStyles', 3); ?> 
+		<?php foreach($twitter as $tweet) : ?> 
+			<li><a href="#"><time><?php echo CMS::relative_date(strtotime($tweet['created_at'])) ?></time></a> <?php echo $tweet['text'] ?></li>
+		<?php endforeach ?> 
+		</ol>
+	</div>
 </section>
