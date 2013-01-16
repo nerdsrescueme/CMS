846f6bd8be6fe2acab75958592589a175a252401
Frank Bardon Jr <frank@nerdsrescue.me>
2013-01-16 09:00:10 -0500

    Added four column template to TBP

---
+++ public/themes/tbp/four-columns.php
@@ -0,0 +1,19 @@
+<article id="content" class="row">
+
+<div class="span3" id="left-column" data-editable="local"><!-- start left-column -->
+	<p>Left column content</p>
+<!-- end left-column --></div>
+
+<div class="span3" id="inner-left-column" data-editable="local"><!-- start inner-left-column -->
+	<p>Inner left column content</p>
+<!-- end inner-left-column --></div>
+
+<div class="span3" id="inner-right-column" data-editable="local"><!-- start inner-right-column -->
+	<p>Inner right column content</p>
+<!-- end inner-right-column --></div>
+
+<div class="span3" id="right-column" data-editable="local"><!-- start right-column -->
+	<p>Right column content</p>
+<!-- end right-column --></div>
+
+</article>
