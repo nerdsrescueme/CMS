91847962b77b2e5750f1322a570e482d65da9ff5
Frank Bardon Jr <frank@nerdsrescue.me>
2013-01-30 09:21:21 -0500

    Updated tf meta tags

--- public/themes/takingflight/template.php
+++ public/themes/takingflight/template.php
@@ -5,8 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<?php if ($layout and $layout == 'home') : ?>
 	<title>Take Flight Learning &mdash; Master the DISC behavior styles</title>
-	<meta name="description" content="Team Builders Plus links development experiences to real-world business issues through team building programs, leadership development and one-on-one performance coaching.">
-	<meta content="keywords" content="team building, leadership development, team builders plus, marlton new jersey">
+	<meta name="description" content="Based on more than two decades of experience with the DISC styles, Take Flight Learning was founded to share DISC with the world. Offering a variety of DISC training programs, products, and services.">
+	<meta content="keywords" content="DISC Styles, behavioral profile, personality profiles, disc training, take flight learning, Taking Flight with DISC, Taking Flight">
 	<?php else : ?>
 	<title><?php echo $page->title ?> - <?php echo $page->subtitle ?></title>
 	<meta name="description" content="<?php echo $page->description ?>">
