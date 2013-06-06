bc4e3b6aaf166d20d325dcf26b90a64a78022f91
Frank Bardon Jr <bardonf@mail.med.upenn.edu>
2013-06-06 12:26:05 -0400

    Added landing tbp template

---
+++ public/themes/tbp/landing.php
@@ -0,0 +1,19 @@
+<article id="content" class="row">
+	<div class="span6" id="main" data-editable="local"><!-- start main -->
+		<h2>Main Headline</h2>
+		<p>Body content</p>
+	<!-- end main --></div>
+
+	<div id="sidebar" class="span5 offset1">
+		<div id="tb-image" class="navigation snippet" data-editable="local"><!-- start tb-image -->
+			<img src="http://placehold.it/320x180" class="bordered">
+		<!-- end tb-image --></div>
+		<div id="tb-similar" class="navigation snippet" data-editable="local"><!-- start tb-similar -->
+		<h3>Similar Programs</h3>
+		<ol>
+			<li><a href="#">First</a></li>
+			<li><a href="#">Second</a></li>
+		</ol>
+		<!-- end tb-similar --></div>
+	</div>
+</article>
