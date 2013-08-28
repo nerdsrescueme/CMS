c758bf17533647b7d019d3a25bc5c09a96fbdcc3
Frank Bardon Jr <bardonf@mail.med.upenn.edu>
2013-08-28 12:25:02 -0400

    Fixed home page content block

--- public/themes/takingflight/home.php
+++ public/themes/takingflight/home.php
@@ -4,7 +4,7 @@
 	<div id="tf-overview-3" data-editable="local" class="span4"><!-- start tf-overview-3 -->Content<!-- end tf-overview-3 --></div>
 </section>
 <section id="overview" class="row">
-	<div id="tf-overview-1-x" data-editable="local" class="span4"><!-- start tf-overview-1-2 -->Content<!-- end tf-overview-1-2 --></div>
+	<div id="tf-overview-1-2" data-editable="local" class="span4"><!-- start tf-overview-1-2 -->Content<!-- end tf-overview-1-2 --></div>
 	<div id="tf-overview-x" data-editable="local" class="span4"><!-- start tf-overview-x -->Content<!-- end tf-overview-x --></div>
 	<div id="tf-overview-3-x" data-editable="local" class="span4"><!-- start tf-overview-3-x -->Content<!-- end tf-overview-3-x --></div>
 </section>
